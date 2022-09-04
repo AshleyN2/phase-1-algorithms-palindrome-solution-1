7ed1e8f665b0bac603d60acf20e6c9089ae1588b
Ashley <ashleynjeri7@gmail.com>
2022-09-04 12:57:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
+function reverseString(word) {
+  /*
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+  */
+  return word.split("").reverse().join("");
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  return word === reversedWord
 }
 
 /* 
   Add your pseudocode here
+
+  // reverse the input string
+  // if the reversed string is the same as the input
+  //   return true
+  // else
+  //   return false
 */
 
 /*
   Add written explanation of your solution here
+
+(1) Saved on a few variable declarations in reverseString() function using 
+method chaining.
+
+(2) Modified the if/else statement since === will evaluate either true/false
+hence we can simply return the result of that comparison 
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -17,6 +47,15 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
